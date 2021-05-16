fff4f210b7e4ccc6f8eefd23967cbc3b109f8504
Roberto Alves <roberto_junior_sousa@hotmail.com>
2021-05-16 20:18:15 -0300

    feat: creates context for sidebar control

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
 
 import { Profile } from './Profile'
 import { NotificationsNav } from './NotificationsNav';
 import { SourceBox } from './SourceBox';
 import { Logo } from './Logo';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
+import { RiMenuLine } from 'react-icons/ri';
 
 export function Header () {
+  const { onOpen } = useSidebarDrawer()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -22,6 +26,17 @@ export function Header () {
       px="6"
       align="center"
     >
+      {!isWideVersion &&
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        />
+      }
+
       <Logo />
 
       {isWideVersion && <SourceBox />}
